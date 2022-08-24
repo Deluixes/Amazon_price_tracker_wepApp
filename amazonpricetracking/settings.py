912c8005269acf51de7230aa45e1b13a87ba6257
Jerome Daulion <jdaulion@yahoo.fr>
2022-08-24 09:28:56 +0200

    Correctif

--- amazonpricetracking/settings.py
+++ amazonpricetracking/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
+SECRET_KEY = config("SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*(@itd^qbahc)vfzj*taox6drh#!-djkt7_wxj4wqoyizp0s01'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
